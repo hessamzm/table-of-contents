039863197be1af6b4f564a3c192a06870bb62d4d
hessam zm <zaremahmoodih@gmail.com>
2026-09-23 16:01:15 +0330

    refactor: build hierarchical TOC tree

--- includes/TOC/TocBuilder.php
+++ includes/TOC/TocBuilder.php
@@ -14,6 +14,44 @@ final class TocBuilder implements TocBuilderInterface
      */
     public function build(array $headings): HeadingTree
     {
-        return new HeadingTree($headings);
+        /** @var list<HeadingNode> $roots */
+        $roots = [];
+
+        /**
+         * Stack entries contain a reference to the current node's children.
+         *
+         * @var list<array{level:int,children:list<HeadingNode>}> $stack
+         */
+        $stack = [];
+
+        foreach ($headings as $heading) {
+            $node = new HeadingNode($heading);
+
+            while ($stack !== [] && $stack[array_key_last($stack)]['level'] >= $heading->level()) {
+                array_pop($stack);
+            }
+
+            if ($stack === []) {
+                $roots[] = $node;
+                $stack[] = [
+                    'level' => $heading->level(),
+                    'children' => &$roots,
+                ];
+                continue;
+            }
+
+            $parentChildren = &$stack[array_key_last($stack)]['children'];
+            $parentIndex = array_key_last($parentChildren);
+            $parentNode = $parentChildren[$parentIndex];
+            $parentChildren[$parentIndex] = $parentNode->withChild($node);
+
+            $children = $parentChildren[$parentIndex]->children();
+            $stack[] = [
+                'level' => $heading->level(),
+                'children' => &$children,
+            ];
+        }
+
+        return new HeadingTree($roots);
     }
 }
