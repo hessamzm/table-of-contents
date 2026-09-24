a8a770252f7f700744dac888360c7a2e5043a47b
hessam zm <zaremahmoodih@gmail.com>
2026-09-24 14:23:13 +0330

    Phase 5: add includes/Integrations/RankMath/RankMathIntegration.php

---
+++ includes/Integrations/RankMath/RankMathIntegration.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+namespace Hessamzm\TableOfContents\Integrations\RankMath;
+
+defined('ABSPATH') || exit;
+
+final class RankMathIntegration
+{
+    public const PLUGIN_FILE = 'table-of-contents/table-of-contents.php';
+
+    public function boot(): void
+    {
+        if (!$this->isEnabled()) {
+            return;
+        }
+
+        add_filter('rank_math/researches/toc_plugins', [$this, 'registerTocPlugin']);
+    }
+
+    /**
+     * @param array<string,string> $tocPlugins
+     * @return array<string,string>
+     */
+    public function registerTocPlugin(array $tocPlugins): array
+    {
+        $tocPlugins[self::PLUGIN_FILE] = __('Table of Contents', 'table-of-contents');
+
+        return $tocPlugins;
+    }
+
+    private function isEnabled(): bool
+    {
+        if (!defined('RANK_MATH_VERSION')) {
+            return false;
+        }
+
+        return (bool) apply_filters(
+            'hessamzm_toc/rank_math_integration_enabled',
+            true
+        );
+    }
+}
